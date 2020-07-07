19ea19a04dde639da00b2baba29a522c34a54bc9
Alvin Galit <aroblesgalit@gmail.com>
2020-07-07 09:18:13 -0500

    Polished header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,15 +4,21 @@ import "./header.css";
 
 function Header() {
     return (
-        <div className="header-container uk-position-fixed uk-height-1-1">
-            <h1>Header</h1>
-            <ul class="uk-nav uk-nav-default">
-                <li class="uk-active"><Link to="/">about</Link></li>
+        <div className="header-container uk-position-fixed uk-height-1-1 uk-text-right">
+            <Link className="header-logo uk-navbar-item uk-logo" to="/">john doe</Link>
+            <ul className="uk-nav uk-nav-default">
+                <li><Link to="/">about</Link></li>
                 <li><Link to="/">portfolio</Link></li>
                 <li><Link to="/">videos</Link></li>
                 <li><Link to="/">shop</Link></li>
                 <li><Link to="/">contact</Link></li>
             </ul>
+            <div className="social-media-links">
+                <a href="" target="_blank" className="uk-icon-button uk-margin-small-right" uk-icon="youtube"></a>
+                <a href="" target="_blank" className="uk-icon-button uk-margin-small-right" uk-icon="twitter"></a>
+                <a href="" target="_blank" className="uk-icon-button uk-margin-small-right" uk-icon="facebook"></a>
+                <a href="" target="_blank" className="uk-icon-button" uk-icon="instagram"></a>
+            </div>
         </div>
     )
 }
