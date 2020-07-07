c7e73d423fe2279aee77dfd14a9db1be55d5a1a0
Alvin Galit <aroblesgalit@gmail.com>
2020-07-07 17:14:58 -0500

    Created item model for shop

---
+++ models/item.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        lowercase: true
+    },
+    img: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    size: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    print: {
+        type: String,
+        trim: true,
+        required: true,
+        lowercase: true
+    },
+    info: {
+        type: String,
+        trim: true,
+        default: "No info provided."
+    },
+    countInStock: {
+        type: Number,
+        default: 0
+    }
+})
+
+const Item = mongoose.model("Item", itemSchema);
+
+module.exports = Item;
