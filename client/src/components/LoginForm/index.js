0a6c5e14028142a710cb5c08bac6d3c7ebcd8c05
Alvin Galit <aroblesgalit@gmail.com>
2020-07-07 13:36:10 -0500

    Created login form

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -3,8 +3,18 @@ import "./loginForm.css";
 
 function LoginForm() {
     return (
-        <div>
-            <h1>LoginForm</h1>
+        <div className="login-form-container">
+            <h1 className="page-heading uk-text-center">Login</h1>
+            <p className="uk-text-center">Hi John Doe! Welcome back!</p>
+            <form className="uk-form-stacked uk-flex uk-flex-column uk-flex-bottom">
+                <div className="uk-margin uk-width-expand">
+                    <label className="uk-form-label" for="password">password</label>
+                    <div className="uk-form-controls">
+                        <input className="uk-input" id="password" type="password" placeholder="***********" />
+                    </div>
+                </div>
+                <button className="uk-button uk-button-default">log in</button>
+            </form>
         </div>
     )
 }
