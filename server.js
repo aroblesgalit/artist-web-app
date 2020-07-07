8a68a15fb7f71cf540dd2543bc9d1c8abf64657f
Alvin Galit <aroblesgalit@gmail.com>
2020-07-07 14:29:07 -0500

    Created server and connected to mongodb

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const session = require("express-session");
+const passport = require("./config/passport");
+const PORT = process.env.PORT || 3001;
+
+// Define middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// Use sessions to keep track of user's login status
+app.use(session({ secret: "johndoe", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Add routes
+app.use(routes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/johndoe", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+// Start the API server
+app.listen(PORT, function () {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
