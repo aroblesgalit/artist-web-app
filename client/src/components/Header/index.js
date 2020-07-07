b6263103ac50fd3516c473cca7cdbd338da21673
Alvin Galit <aroblesgalit@gmail.com>
2020-07-06 22:29:51 -0500

    Started header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+    return (
+        <div className="header-container">
+            <h1>Header</h1>
+        </div>
+    )
+}
+
+export default Header;
