3cf8a29e578eec786cd51580e01eab3d1ae7c0f6
Alvin Galit <aroblesgalit@gmail.com>
2020-07-08 17:38:56 -0500

    Added logic for add cart button on the card

--- client/src/components/ItemCard/index.js
+++ client/src/components/ItemCard/index.js
@@ -15,29 +15,35 @@ function ItemCard(props) {
                     {
                         value => {
                             return (
-                                <Link to={`/shop/${_id}`}>
-                                    <div className="item-img-container uk-card-media-top">
-                                        <img 
-                                            src={img} 
-                                            alt={name}
-                                            onClick={() => value.handleDetail(_id)} 
-                                        />
+                                <React.Fragment>
+                                    <Link to={`/shop/${_id}`}>
+                                        <div className="item-img-container uk-card-media-top">
+                                            <img
+                                                src={img}
+                                                alt={name}
+                                                onClick={() => value.handleDetail(_id)}
+                                            />
+                                        </div>
+                                    </Link>
+                                    <div className="uk-card-body uk-padding-small uk-position-relative">
+                                        <h3 className="item-name uk-card-title">{name}</h3>
+                                        <p className="item-size">{size}</p>
+                                        <span className="item-price">${price}</span>
+                                        <button
+                                            className="cart-btn"
+                                            disabled={inCart}
+                                            onClick={() => value.addToCart(_id)}
+                                        >
+                                            {
+                                                inCart ? "in cart" : "add to cart"
+                                            }
+                                        </button>
                                     </div>
-                                </Link>
+                                </React.Fragment>
                             )
                         }
                     }
                 </ItemConsumer>
-                <div className="uk-card-body uk-padding-small uk-position-relative">
-                    <h3 className="item-name uk-card-title">{name}</h3>
-                    <p className="item-size">{size}</p>
-                    <span className="item-price">${price}</span>
-                    <button className="cart-btn" disabled={inCart}>
-                        {
-                            inCart ? "in cart" : "add to cart"
-                        }
-                    </button>
-                </div>
             </div>
         </div>
     )
