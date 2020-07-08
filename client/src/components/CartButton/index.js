72911e27d19a078a93096cd39c95aa97b0f985d2
Alvin Galit <aroblesgalit@gmail.com>
2020-07-08 18:29:38 -0500

    Used ItemConsumer to only show the cart button link when the cart isn't empty

--- client/src/components/CartButton/index.js
+++ client/src/components/CartButton/index.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./cartButton.css";
+import { ItemConsumer } from "../../utils/ItemContext";
 
 function CartButton() {
     return (
-        <Link to="/cart">
-            <div className="cart-link uk-flex uk-flex-center uk-flex-middle">
-                <span uk-icon="icon: cart"></span>
-            </div>
-        </Link>
+        <ItemConsumer>
+            {
+                value => {
+                    if (value.cart.length > 0) {
+                        return (
+                            <Link to="/cart">
+                                <div className="cart-link uk-flex uk-flex-center uk-flex-middle">
+                                    <span uk-icon="icon: cart"></span>
+                                </div>
+                            </Link>
+                        )
+                    } else {
+                        return;
+                    }
+                }
+            }
+        </ItemConsumer>
     )
 }
 
