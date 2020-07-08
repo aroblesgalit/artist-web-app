8cb7f4626168308e5c908e58e9a9fa5935ef45af
Alvin Galit <aroblesgalit@gmail.com>
2020-07-08 16:43:49 -0500

    Used ItemContext to get subtotal values

--- client/src/components/CartSubtotal/index.js
+++ client/src/components/CartSubtotal/index.js
@@ -1,27 +1,34 @@
 import React from "react";
 import "./cartSubtotal.css";
+import { ItemConsumer } from "../../utils/ItemContext";
 
 function CartSubtotal() {
     return (
-        <tr>
-            <td></td>
-            <td></td>
-            <td className="subtotal-info">
-                <div className="uk-flex uk-flex-right">
-                    <div className="subtotal-labels uk-margin-right">
-                        <div className="uk-flex uk-flex-bottom uk-flex-right">subtotal</div>
-                        <div className="uk-flex uk-flex-bottom uk-flex-right">shipping</div>
-                        <div className="uk-flex uk-flex-bottom uk-flex-right"><b>total</b></div>
-                    </div>
-                    <div className="subtotal-values">
-                        <div className="uk-flex uk-flex-bottom uk-flex-right">$30</div>
-                        <div className="uk-flex uk-flex-bottom uk-flex-right">$4</div>
-                        <div className="uk-flex uk-flex-bottom uk-flex-right"><b>$34</b></div>
-                    </div>
-                </div>
-                <button className="primary-btn">paypal checkout</button>
-            </td>
-        </tr>
+        <ItemConsumer>
+            {
+                value => {
+                    return <tr>
+                        <td></td>
+                        <td></td>
+                        <td className="subtotal-info">
+                            <div className="uk-flex uk-flex-right">
+                                <div className="subtotal-labels uk-margin-right">
+                                    <div className="uk-flex uk-flex-bottom uk-flex-right">subtotal</div>
+                                    <div className="uk-flex uk-flex-bottom uk-flex-right">shipping</div>
+                                    <div className="uk-flex uk-flex-bottom uk-flex-right"><b>total</b></div>
+                                </div>
+                                <div className="subtotal-values">
+                                    <div className="uk-flex uk-flex-bottom uk-flex-right">${value.cartSubtotal}</div>
+                                    <div className="uk-flex uk-flex-bottom uk-flex-right">${value.cartShipping}</div>
+                                    <div className="uk-flex uk-flex-bottom uk-flex-right"><b>${value.cartTotal}</b></div>
+                                </div>
+                            </div>
+                            <button className="primary-btn">paypal checkout</button>
+                        </td>
+                    </tr>
+                }
+            }
+        </ItemConsumer>
     )
 }
 
