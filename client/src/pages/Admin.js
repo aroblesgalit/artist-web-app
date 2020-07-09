457bda0b8818af182004853112ac7831ae044a7f
Alvin Galit <aroblesgalit@gmail.com>
2020-07-09 12:19:17 -0500

    Added logic for showing content based on active tab

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -15,10 +15,10 @@ function Admin() {
                     <li className={`${activeTab === "about" ? "active" : ""}`} onClick={() => setActiveTab("about")}>about</li>
                 </ul>
             </div>
-            <div><h1>portfolio</h1></div>
-            <div><h1>videos</h1></div>
-            <div><h1>shop</h1></div>
-            <div><h1>about</h1></div>
+            <div className={`portfolio-content ${activeTab === "portfolio" ? "show" : "hide"}`}><h1>portfolio</h1></div>
+            <div className={`videos-content ${activeTab === "videos" ? "show" : "hide"}`}><h1>videos</h1></div>
+            <div className={`shop-content ${activeTab === "shop" ? "show" : "hide"}`}><h1>shop</h1></div>
+            <div className={`about-content ${activeTab === "about" ? "show" : "hide"}`}><h1>about</h1></div>
         </div>
     )
 }
