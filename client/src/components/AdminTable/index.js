fd5c5086080ee52a7b8a796090cf58f70f717c71
Alvin Galit <aroblesgalit@gmail.com>
2020-07-09 13:31:27 -0500

    Created component for admin table

---
+++ client/src/components/AdminTable/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./adminTable.css";
+
+function AdminTable(props) {
+
+    const { tableHeads, children } = props;
+
+    return (
+        <table className="admin-table uk-table uk-table-responsive uk-table-divider uk-table-middle uk-width-4-5@l">
+            <thead>
+                <tr>
+                    {
+                        tableHeads.map(header => {
+                            return <th>{header}</th>
+                        })
+                    }
+                </tr>
+            </thead>
+            <tbody>
+                {children}
+            </tbody>
+        </table>
+    )
+}
+
+export default AdminTable;
--- client/src/components/ShopAdmin/index.js
+++ client/src/components/ShopAdmin/index.js
@@ -1,12 +1,23 @@
 import React from "react";
+import AdminTable from "../AdminTable";
 
 function ShopAdmin(props) {
 
     const { activeTab } = props;
+    const tableHeads = ["image", "name/size/print", "count in stock", "sold", "price", "actions"];
 
     return (
         <div className={`shop-content ${activeTab === "shop" ? "show" : "hide"}`}>
-            <h1>shop</h1>
+            <AdminTable tableHeads={tableHeads}>
+                <tr>
+                    <td>https://www.google.com/image.png</td>
+                    <td>hello world</td>
+                    <td>20</td>
+                    <td>15</td>
+                    <td>$30</td>
+                    <td>view / delete</td>
+                </tr>
+            </AdminTable>
         </div>
     )
 }
