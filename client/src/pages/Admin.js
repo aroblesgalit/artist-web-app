764ebf5370ebb258870f2440ae1d84981e57472c
Alvin Galit <aroblesgalit@gmail.com>
2020-07-09 12:15:52 -0500

    Created tabs for admin and added logic for setting the active tab

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./pages.css";
 
 function Admin() {
 
+    const [activeTab, setActiveTab] = useState("portfolio");
+
     return (
         <div className="main-container">
-            <h1>Admin</h1>
+            <div className="admin-tabs">
+                <ul uk-tab="true">
+                    <li className={`${activeTab === "portfolio" ? "active" : ""}`} onClick={() => setActiveTab("portfolio")}>portfolio</li>
+                    <li className={`${activeTab === "videos" ? "active" : ""}`} onClick={() => setActiveTab("videos")}>videos</li>
+                    <li className={`${activeTab === "shop" ? "active" : ""}`} onClick={() => setActiveTab("shop")}>shop</li>
+                    <li className={`${activeTab === "about" ? "active" : ""}`} onClick={() => setActiveTab("about")}>about</li>
+                </ul>
+            </div>
+            <div><h1>portfolio</h1></div>
+            <div><h1>videos</h1></div>
+            <div><h1>shop</h1></div>
+            <div><h1>about</h1></div>
         </div>
     )
 }
