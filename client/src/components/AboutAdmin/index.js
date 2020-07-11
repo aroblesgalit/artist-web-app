dd73057b6ac8e479f81aa9841456a03562b9d592
Alvin Galit <aroblesgalit@gmail.com>
2020-07-11 01:48:49 -0500

    Setup about admin page

--- client/src/components/AboutAdmin/index.js
+++ client/src/components/AboutAdmin/index.js
@@ -1,12 +1,180 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import "./aboutAdmin.css";
 
 function AboutAdmin(props) {
 
     const { activeTab } = props;
 
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const phoneRef = useRef();
+    const aboutRef = useRef();
+    const imgHomeRef = useRef();
+    const imgAboutTopRef = useRef();
+    const imgAboutBotRef = useRef();
+
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        about: "",
+        socialMedias: [],
+        imgHome: "",
+        imgAboutTop: "",
+        imgAboutBot: ""
+    });
+
     return (
         <div className={`about-content ${activeTab === "about" ? "show" : "hide"}`}>
-            <h1>about</h1>
+            <form className="about-admin uk-grid uk-width-4-5@l" uk-grid="true">
+                <div className="uk-width-1-2@m">
+                    <div className="uk-margin">
+                        <label className="uk-form-label" htmlFor="user-name">name</label>
+                        <div className="uk-form-controls">
+                            <input
+                                className="uk-input"
+                                id="user-name"
+                                type="text"
+                                placeholder="john doe"
+                                required={true}
+                                ref={nameRef}
+                                value={user.name}
+                                onChange={() => setUser({
+                                    ...user,
+                                    name: nameRef.current.value
+                                })}
+                            />
+                        </div>
+                    </div>
+                    <div className="uk-margin">
+                        <label className="uk-form-label" htmlFor="user-email">email</label>
+                        <div className="uk-form-controls">
+                            <input
+                                className="uk-input"
+                                id="user-email"
+                                type="email"
+                                placeholder="johndoe@email.com"
+                                required={true}
+                                ref={emailRef}
+                                value={user.email}
+                                onChange={() => setUser({
+                                    ...user,
+                                    email: emailRef.current.value
+                                })}
+                            />
+                        </div>
+                    </div>
+                    <div className="uk-margin">
+                        <label className="uk-form-label" htmlFor="user-phone">phone</label>
+                        <div className="uk-form-controls">
+                            <input
+                                className="uk-input"
+                                id="user-phone"
+                                type="tel"
+                                placeholder="555.555.5555"
+                                required={true}
+                                ref={phoneRef}
+                                value={user.phone}
+                                onChange={() => setUser({
+                                    ...user,
+                                    phone: phoneRef.current.value
+                                })}
+                            />
+                        </div>
+                    </div>
+                    <div className="uk-margin">
+                        <label className="uk-form-label" htmlFor="user-about">about</label>
+                        <div className="uk-form-controls">
+                            <textarea
+                                className="uk-input"
+                                id="user-about"
+                                type="message"
+                                placeholder="My story. Here it is..."
+                                ref={aboutRef}
+                                value={user.about}
+                                onChange={() => setUser({
+                                    ...user,
+                                    about: aboutRef.current.value
+                                })}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="uk-width-1-2@m">
+                    <div className="uk-margin">
+                        <label className="uk-form-label" htmlFor="user-imgHome">home image</label>
+                        <div className="uk-form-controls">
+                            <input
+                                className="uk-input"
+                                id="user-imgHome"
+                                type="url"
+                                placeholder="www.site.com/image.png"
+                                required={true}
+                                ref={imgHomeRef}
+                                value={user.img}
+                                onChange={() => setUser({
+                                    ...user,
+                                    imgHome: imgHomeRef.current.value
+                                })}
+                            />
+                        </div>
+                    </div>
+                    <div className="uk-margin">
+                        <label className="uk-form-label" htmlFor="user-imgAboutTop">about top image</label>
+                        <div className="uk-form-controls">
+                            <input
+                                className="uk-input"
+                                id="user-imgAboutTop"
+                                type="url"
+                                placeholder="www.site.com/image.png"
+                                required={true}
+                                ref={imgAboutTopRef}
+                                value={user.imgAboutTop}
+                                onChange={() => setUser({
+                                    ...user,
+                                    imgAboutTop: imgAboutTopRef.current.value
+                                })}
+                            />
+                        </div>
+                    </div>
+                    <div className="uk-margin">
+                        <label className="uk-form-label" htmlFor="user-imgAboutBot">about bottom image</label>
+                        <div className="uk-form-controls">
+                            <input
+                                className="uk-input"
+                                id="user-imgAboutBot"
+                                type="url"
+                                placeholder="www.site.com/image.png"
+                                required={true}
+                                ref={imgAboutBotRef}
+                                value={user.imgAboutBot}
+                                onChange={() => setUser({
+                                    ...user,
+                                    imgAboutBot: imgAboutBotRef.current.value
+                                })}
+                            />
+                        </div>
+                    </div>
+                    <div className="uk-margin">
+                        <button
+                            className="primary-btn"
+                        // onClick={(e) => {
+                        //     value.updateItem(e, _id, {
+                        //         name: item.name,
+                        //         img: item.img,
+                        //         price: item.price,
+                        //         size: item.size,
+                        //         print: item.print,
+                        //         info: item.info,
+                        //         countInStock: item.countInStock
+                        //     });
+                        // }}
+                        >
+                            <span uk-icon="file-edit" className="uk-margin-small-right" />save changes
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
