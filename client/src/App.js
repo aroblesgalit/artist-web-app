66484da02511890fb075d1fa86d07b61166b03a8
Alvin Galit <aroblesgalit@gmail.com>
2020-07-11 17:32:38 -0500

    Updated login form with about consumer

--- client/src/App.js
+++ client/src/App.js
@@ -19,74 +19,77 @@ import PublicRoute from "./components/PublicRoute";
 import { ItemProvider } from "./utils/ItemContext";
 import { UserProvider } from "./utils/UserContext";
 import { ArtProvider } from "./utils/ArtContext";
+import { AboutProvider } from "./utils/AboutContext";
 
 function App() {
   return (
     <ArtProvider>
       <ItemProvider>
         <UserProvider>
-          <Router>
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                <Home />
-                <CartButton />
-              </Route>
-              <Route exact path="/shop">
-                <Shop />
-                <CartButton />
-              </Route>
-              <Route path="/shop/:id">
-                <ItemDetail />
-                <CartButton />
-              </Route>
-              <Route path="/cart">
-                <Cart />
-              </Route>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/portfolio">
-                <Portfolio />
-              </Route>
-              <Route path="/admin-login">
-                <PublicRoute component={Login} />
-                {
-                  // isLoggedIn ? <Redirect to="/admin" /> : <Login />
-                }
-              </Route>
-              <Route exact path="/admin">
-                <ProtectedRoute component={Admin} />
-                {
-                  // isLoggedIn ? <Admin /> : <Redirect to="/admin-login" />
-                }
-              </Route>
-              <Route path="/admin/shop-add">
-                <ProtectedRoute component={AddShop} />
-                {
-                  // isLoggedIn ? <AddShop /> : <Redirect to="/admin" />
-                }
-              </Route>
-              <Route path="/admin/shop-view">
-                <ProtectedRoute component={ViewShop} />
-                {
-                  // isLoggedIn ? <ViewShop /> : <Redirect to="/admin" />
-                }
-              </Route>
-              <Route path="/admin/portfolio-view">
-                <ProtectedRoute component={ViewPortfolio} />
-                {
-                  // isLoggedIn ? <ViewPortfolio /> : <Redirect to="/admin" />
-                }
-              </Route>
-              <Route path="/admin/portfolio-add">
-                <ProtectedRoute component={AddPortfolio} />
-                {
-                  // isLoggedIn ? <AddPortfolio /> : <Redirect to="/admin" />
-                }
-              </Route>
-            </Switch>
-          </Router>
+          <AboutProvider>
+            <Router>
+              <Header />
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                  <CartButton />
+                </Route>
+                <Route exact path="/shop">
+                  <Shop />
+                  <CartButton />
+                </Route>
+                <Route path="/shop/:id">
+                  <ItemDetail />
+                  <CartButton />
+                </Route>
+                <Route path="/cart">
+                  <Cart />
+                </Route>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/portfolio">
+                  <Portfolio />
+                </Route>
+                <Route path="/admin-login">
+                  <PublicRoute component={Login} />
+                  {
+                    // isLoggedIn ? <Redirect to="/admin" /> : <Login />
+                  }
+                </Route>
+                <Route exact path="/admin">
+                  <ProtectedRoute component={Admin} />
+                  {
+                    // isLoggedIn ? <Admin /> : <Redirect to="/admin-login" />
+                  }
+                </Route>
+                <Route path="/admin/shop-add">
+                  <ProtectedRoute component={AddShop} />
+                  {
+                    // isLoggedIn ? <AddShop /> : <Redirect to="/admin" />
+                  }
+                </Route>
+                <Route path="/admin/shop-view">
+                  <ProtectedRoute component={ViewShop} />
+                  {
+                    // isLoggedIn ? <ViewShop /> : <Redirect to="/admin" />
+                  }
+                </Route>
+                <Route path="/admin/portfolio-view">
+                  <ProtectedRoute component={ViewPortfolio} />
+                  {
+                    // isLoggedIn ? <ViewPortfolio /> : <Redirect to="/admin" />
+                  }
+                </Route>
+                <Route path="/admin/portfolio-add">
+                  <ProtectedRoute component={AddPortfolio} />
+                  {
+                    // isLoggedIn ? <AddPortfolio /> : <Redirect to="/admin" />
+                  }
+                </Route>
+              </Switch>
+            </Router>
+          </AboutProvider>
         </UserProvider>
       </ItemProvider>
     </ArtProvider>
--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,33 +1,41 @@
 import React, { useRef } from "react";
 import "./loginForm.css";
 import { UserConsumer } from "../../utils/UserContext";
+import { AboutConsumer } from "../../utils/AboutContext";
 
 function LoginForm() {
 
     const passwordRef = useRef();
 
     return (
-        <UserConsumer>
-            {
-                value => {
-                    return (
-                        <div className="login-form-container">
-                            <h1 className="page-heading uk-text-center">Login</h1>
-                            <p className="uk-text-center">Hi {value.userInfo.name}! Welcome back!</p>
-                            <form className="uk-form-stacked uk-flex uk-flex-column uk-flex-bottom">
-                                <div className="uk-margin uk-width-expand">
-                                    <label className="uk-form-label" htmlFor="password">password</label>
-                                    <div className="uk-form-controls">
-                                        <input className="uk-input" id="password" type="password" placeholder="***********" ref={passwordRef} />
-                                    </div>
-                                </div>
-                                <button className="uk-button uk-button-default" onClick={(e) => value.handleLogin(e, passwordRef.current.value)}>log in</button>
-                            </form>
-                        </div>
-                    )
+
+
+        <div className="login-form-container">
+            <h1 className="page-heading uk-text-center">Login</h1>
+            <AboutConsumer>
+                {
+                    value => {
+                        return <p className="uk-text-center">Hi {value.name}! Welcome back!</p>
+                    }
                 }
-            }
-        </UserConsumer>
+            </AboutConsumer>
+
+            <form className="uk-form-stacked uk-flex uk-flex-column uk-flex-bottom">
+                <div className="uk-margin uk-width-expand">
+                    <label className="uk-form-label" htmlFor="password">password</label>
+                    <div className="uk-form-controls">
+                        <input className="uk-input" id="password" type="password" placeholder="***********" ref={passwordRef} />
+                    </div>
+                </div>
+                <UserConsumer>
+                    {
+                        value => {
+                            return <button className="uk-button uk-button-default" onClick={(e) => value.handleLogin(e, passwordRef.current.value)}>log in</button>
+                        }
+                    }
+                </UserConsumer>
+            </form>
+        </div>
     )
 }
 
