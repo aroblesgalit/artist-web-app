5e88bdfe253311918ab3a47490eed96b93b96e24
Alvin Galit <aroblesgalit@gmail.com>
2020-07-10 19:15:15 -0500

    Set up api routes for art

---
+++ routes/api/art.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const artsController = require("../../controllers/artsController");
+
+// Matches with "/api/arts"
+router.route("/")
+    .get(artsController.getAllArts)
+    .post(artsController.addArt)
+
+// Matches with "/api/arts/:id"
+router.route("/:id")
+    .put(artsController.updateArt)
+    .delete(artsController.deleteArt)
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./user");
 const itemRoutes = require("./item");
+const artRoutes = require("./art");
 
 router.use("/user", userRoutes);
 router.use("/items", itemRoutes);
+router.use("/arts", artRoutes);
 
 module.exports = router;
