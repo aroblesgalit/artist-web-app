1cbf136761b190ad2ab8992d45119549d1d7eaa7
Alvin Galit <aroblesgalit@gmail.com>
2020-07-11 11:13:45 -0500

    Updated user schema with more fields

--- models/user.js
+++ models/user.js
@@ -4,16 +4,54 @@ const bcrypt = require("bcrypt");
 const SALT_WORK_FACTOR = 10;
 
 const userSchema = new Schema({
-    username: {
+    name: {
         type: String,
         trim: true,
-        unique: true
+        unique: true,
+        lowercase: true,
+        default: "john doe"
     },
     password: {
         type: String,
         required: true,
         trim: true,
         validate: [({ length }) => length >= 6, "Password should be longer."]
+    },
+    email: {
+        type: String,
+        trim: true,
+        match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+        lowercase: true,
+        default: "johndoe@email.com"
+    },
+    phone: {
+        type: Number,
+        trim: true,
+        match:[/\d{10}/],
+        default: 5555555555
+    },
+    about: {
+        type: String,
+        trim: true,
+        default: "Here's my story..."
+    },
+    socialMedias: {
+        type: Array
+    },
+    imgHome: {
+        type: String,
+        trim: true,
+        default: "https://via.placeholder.com/900x1230"
+    },
+    imgAboutTop: {
+        type: String,
+        trim: true,
+        default: "https://via.placeholder.com/900x700"
+    },
+    imgAboutBot: {
+        type: String,
+        trim: true,
+        default: "https://via.placeholder.com/900x700"
     }
 });
 
