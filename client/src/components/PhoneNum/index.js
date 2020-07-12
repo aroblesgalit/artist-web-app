204d1b21d886118f7860fb2e2d2b26ea5320a358
Alvin Galit <aroblesgalit@gmail.com>
2020-07-11 19:39:48 -0500

    Created a component for the phone number

---
+++ client/src/components/PhoneNum/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function PhoneNum(props) {
+
+    const { phone } = props;
+
+    let phoneToArr = phone.toString().split("");
+    phoneToArr.splice(3, 0, "-");
+    phoneToArr.splice(7, 0, "-");
+
+    return (
+        <a href={`tel:+${phone}`} className="uk-margin-small">{phoneToArr}</a>
+    )
+}
+
+export default PhoneNum;
