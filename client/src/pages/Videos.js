a06afc55aeb26d45325ba598bfd377883522e802
Alvin Galit <aroblesgalit@gmail.com>
2020-07-12 15:50:54 -0500

    Used video consumer in video spage

--- client/src/pages/Videos.js
+++ client/src/pages/Videos.js
@@ -1,42 +1,27 @@
 import React from "react";
 import "./pages.css";
 import Video from "../components/Video";
+import { VideoConsumer } from "../utils/VideoContext";
 
 function Videos() {
-
-    const videos = [
-        {
-            _id: "1",
-            title: "video 1",
-            url: "https://www.youtube-nocookie.com/embed/c2pz2mlSfXA"
-        },
-        {
-            _id: "2",
-            title: "video 2",
-            url: "https://www.youtube.com/embed/g0qaTzjNZ2Y"
-        },
-        {
-            _id: "3",
-            title: "video 3",
-            url: "https://www.youtube.com/embed/ILXhHv-0V9Q"
-        }
-    ]
-
-
     return (
         <div className="main-container">
             <div className="videos-container uk-flex uk-flex-column">
-                {
-                    videos.map(video => {
-                        return (
-                            <Video
-                                key={video._id}
-                                title={video.title}
-                                url={video.url}
-                            />
-                        )
-                    })
-                }
+                <VideoConsumer>
+                    {
+                        value => {
+                            return value.items.map(video => {
+                                return (
+                                    <Video
+                                        key={video._id}
+                                        title={video.title}
+                                        url={video.url}
+                                    />
+                                )
+                            })
+                        }
+                    }
+                </VideoConsumer>
             </div>
         </div>
     )
