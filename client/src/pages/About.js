650e42cdd3dfea79c9c70bfc167ab08fa428ccc5
Alvin Galit <aroblesgalit@gmail.com>
2020-07-11 19:37:31 -0500

    Turned contact links into anchor tags

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -8,20 +8,23 @@ function About() {
             {
                 value => {
                     const { imgAboutTop, name, about, email, phone, socialMedias, imgAboutBot } = value;
+                    let phoneToArr = phone.toString().split("");
+                    phoneToArr.splice(3, 0, "-");
+                    phoneToArr.splice(7, 0, "-");
                     return (
                         <div className="main-container about-container">
                             <img src={imgAboutTop} alt={name} />
                             <div className="uk-margin-large uk-width-1-2@l">
                                 <p>{about}</p>
                             </div>
-                            <div className="uk-margin-large uk-width-1-2@l">
+                            <div className="uk-margin-large uk-flex uk-flex-column uk-width-1-2@l">
                                 <h4>connect with me</h4>
-                                <p className="uk-margin-small">{email}</p>
-                                <p className="uk-margin-small">{phone}</p>
+                                <a href={`mailto:${email}`} className="uk-margin-small">{email}</a>
+                                <a href={`tel:+${phone}`} className="uk-margin-small">{phoneToArr}</a>
                                 {
                                     socialMedias ? (
                                         socialMedias.map(social => {
-                                            return <p key={social.id} className="uk-margin-small">{social.link}</p>
+                                            return <a href={social.link} target="_blank" key={social.id} className="uk-margin-small">{social.link}</a>
                                         })
                                     ) : ""
 
