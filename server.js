8eeb6e939ec00e42b092cd58daf82d4bd4398e92
Alvin Galit <aroblesgalit@gmail.com>
2020-07-12 11:39:12 -0500

    Adjusted line of codes to fix routing issues

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(routes);
 
 // Serving static files
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 // Connect to MongoDB
