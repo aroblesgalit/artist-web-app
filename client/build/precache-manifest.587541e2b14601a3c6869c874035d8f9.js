8aef491f2429d8a3838e2006e9a52a5d34f756dc
Alvin Galit <aroblesgalit@gmail.com>
2020-07-13 17:33:17 -0500

    Removed build from gitignore

---
+++ client/build/precache-manifest.587541e2b14601a3c6869c874035d8f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f2bc8c5f33ac029b50adfd4e30b51a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "054429a65735d121f8ab",
+    "url": "/static/css/main.5619df28.chunk.css"
+  },
+  {
+    "revision": "90620dffefdd8af6dec8",
+    "url": "/static/js/2.399bec9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.399bec9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "054429a65735d121f8ab",
+    "url": "/static/js/main.e044bee5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
