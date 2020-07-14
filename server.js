a7a9c23b78810543eaf4b9d36844a2535910d464
Alvin Galit <aroblesgalit@gmail.com>
2020-07-13 22:46:59 -0500

    Changed serve static sites

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("./client/build"));
+    app.use(express.static("client/build"));
 }
 
 // Use sessions to keep track of user's login status
