ea89fe94629a55a163bfce0bec958382bb4b8b14
Alvin Galit <aroblesgalit@gmail.com>
2020-07-22 21:20:17 -0500

    Added sold out badge on item card for when it's out of stock

--- client/src/components/ItemCard/index.js
+++ client/src/components/ItemCard/index.js
@@ -6,7 +6,7 @@ import { ItemConsumer } from "../../utils/ItemContext";
 
 function ItemCard(props) {
 
-    const { _id, name, size, img, price, inCart } = props.item;
+    const { _id, name, size, img, price, inCart, soldOut } = props.item;
 
     return (
         <div>
@@ -17,25 +17,34 @@ function ItemCard(props) {
                             return (
                                 <React.Fragment>
                                     <Link to={`/shop/${_id}`}>
-                                        <div className="item-img-container uk-card-media-top">
+                                        <div className="item-img-container uk-card-media-top uk-position-relative">
                                             <img
                                                 src={img}
                                                 alt={name}
                                                 onClick={() => value.handleDetail(_id)}
                                             />
+                                            {soldOut ? <div className="image-tint" onClick={() => value.handleDetail(_id)}></div> : ""}
                                         </div>
                                     </Link>
                                     <div className="uk-card-body uk-padding-small uk-position-relative">
                                         <h3 className="item-name uk-card-title">{name}</h3>
                                         <p className="item-size">{size}</p>
-                                        <span className="item-price">${price}</span>
+                                        {
+                                            soldOut ? (
+                                                <span className="item-sold-out">sold out</span>
+                                            ) : (
+                                                    <span className="item-price">${price}</span>
+                                                )
+                                        }
                                         <button
                                             className="cart-btn"
-                                            disabled={inCart}
+                                            disabled={inCart || soldOut}
                                             onClick={() => value.addToCart(_id)}
                                         >
                                             {
-                                                inCart ? "in cart" : "add to cart"
+                                                soldOut ? "out of stock" : (
+                                                    inCart ? "in cart" : "add to cart"
+                                                )
                                             }
                                         </button>
                                     </div>
